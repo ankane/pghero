afb706e40a6f87e493c4d0f0836648a0860336d2
Andrey Chernih <andrey.chernih@gmail.com>
2014-08-02 19:35:36 +0400

    Sort indexes table properly

--- app/assets/javascript/pghero.js
+++ app/assets/javascript/pghero.js
@@ -1,9 +1,9 @@
 //= require jquery
 //= require dataTables/jquery.dataTables
 
-$.fn.dataTable.ext.order['data-bytes'] = function (settings, col) {
+$.fn.dataTable.ext.order['data-value'] = function (settings, col) {
   return this.api().column(col, {order: 'index'}).nodes().map(function(td, i) {
-    return parseInt($(td).data('byte-size'));
+    return parseInt($(td).data('value'));
   });
 };
 
@@ -16,14 +16,15 @@ $.extend($.fn.dataTable.defaults, {
 $(function() {
   $('table.space').dataTable({
     columnDefs: [
-      { 'orderDataType': 'data-bytes', 'targets': [2], 'type': 'numeric' }
+      { 'orderDataType': 'data-value', 'targets': 2, 'type': 'numeric' }
     ],
     order: [[2, 'desc']]
   });
 
   $('table.indexes').dataTable({
     columnDefs: [
-      { 'targets': [1, 2], 'type': 'numeric' }
+      { 'orderDataType': 'data-value', 'targets': 2, 'type': 'numeric' },
+      { 'targets': 1, 'orderDataType': 'numeric' }
     ],
     order: [[2, 'desc']]
   });
