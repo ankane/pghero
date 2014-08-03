377c797459419472eee893356697f6ef3b8157ba
Andrew Kane <acekane1@gmail.com>
2014-08-03 14:32:47 -0700

    Better tests

--- test/pghero_test.rb
+++ test/pghero_test.rb
@@ -8,17 +8,17 @@ class TestPgHero < Minitest::Test
 
   def test_explain
     User.create!
-    PgHero.explain("DELETE FROM users")
+    PgHero.explain("ANALYZE DELETE FROM users")
     assert_equal 1, User.count
   end
 
   def test_explain_multiple_statements
     User.create!
     if ActiveRecord::VERSION::MAJOR == 3 and ActiveRecord::VERSION::MINOR < 2
-      PgHero.explain("DELETE FROM users; COMMIT")
+      PgHero.explain("ANALYZE DELETE FROM users; DELETE FROM users; COMMIT")
       assert_equal 1, User.count
     else
-      assert_raises(ActiveRecord::StatementInvalid){ PgHero.explain("DELETE FROM users; COMMIT") }
+      assert_raises(ActiveRecord::StatementInvalid){ PgHero.explain("ANALYZE DELETE FROM users; DELETE FROM users; COMMIT") }
     end
   end
 
