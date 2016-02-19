2a2988a7d73edbf141cf00565571a6651f2c77ae
Andrew Kane <acekane1@gmail.com>
2016-02-19 02:19:48 -0800

    Skip pg_stat_statements test on travis

--- test/suggested_indexes_test.rb
+++ test/suggested_indexes_test.rb
@@ -6,6 +6,9 @@ class SuggestedIndexesTest < Minitest::Test
   end
 
   def test_basic
+    # no pg_stat_statements
+    skip if ENV["TRAVIS_CI"]
+
     User.where(email: "person1@example.org").first
     assert_equal [{table: "users", columns: ["email"]}], PgHero.suggested_indexes.map { |q| q.except(:queries, :details) }
   end
