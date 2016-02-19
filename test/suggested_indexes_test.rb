fc5731d45b4626102cc62b4475ae282c4b97eec6
Andrew Kane <acekane1@gmail.com>
2016-02-19 02:09:54 -0800

    Removed commented code

--- test/suggested_indexes_test.rb
+++ test/suggested_indexes_test.rb
@@ -7,8 +7,6 @@ class SuggestedIndexesTest < Minitest::Test
 
   def test_basic
     User.where(email: "person1@example.org").first
-    # User.where(email: "person1@example.org", city_id: 1).first
-    # User.where(city_id: 1).to_a
     assert_equal [{table: "users", columns: ["email"]}], PgHero.suggested_indexes.map { |q| q.except(:queries, :details) }
   end
 end
