a2fdb4fb4554356bb674727ac5e44e1eebf574d6
Andrew Kane <acekane1@gmail.com>
2016-02-28 22:32:43 -0800

    Better query for replication lag

--- lib/pghero/methods/replica.rb
+++ lib/pghero/methods/replica.rb
@@ -5,9 +5,17 @@ module PgHero
         select_all("SELECT setting FROM pg_settings WHERE name = 'hot_standby'").first["setting"] == "on"
       end
 
-      # http://www.niwi.be/2013/02/16/replication-status-in-postgresql/
+      # http://www.postgresql.org/message-id/CADKbJJWz9M0swPT3oqe8f9+tfD4-F54uE6Xtkh4nERpVsQnjnw@mail.gmail.com
       def replication_lag
-        select_all("SELECT EXTRACT(EPOCH FROM NOW() - pg_last_xact_replay_timestamp()) AS replication_lag").first["replication_lag"].to_f
+        select_all(<<-SQL
+          SELECT
+            CASE
+              WHEN pg_last_xlog_receive_location() = pg_last_xlog_replay_location() THEN 0
+              ELSE EXTRACT (EPOCH FROM NOW() - pg_last_xact_replay_timestamp())
+            END
+          AS replication_lag
+        SQL
+        ).first["replication_lag"].to_f
       end
     end
   end
