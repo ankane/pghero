cab91c9be3c5df994bbce5531824bbcb07993d27
Andrew Kane <acekane1@gmail.com>
2016-10-07 02:10:55 -0700

    Better blocked queries

--- lib/pghero/methods/queries.rb
+++ lib/pghero/methods/queries.rb
@@ -62,20 +62,21 @@ module PgHero
           SELECT
             bl.pid AS blocked_pid,
             a.usename AS blocked_user,
-            ka.query AS current_or_recent_query_in_blocking_process,
-            ka.state AS state_of_blocking_process,
-            age(now(), ka.query_start) AS blocking_duration,
+            a.query AS blocked_query,
+            age(now(), a.query_start) AS blocked_duration,
+            bl.mode,
             kl.pid AS blocking_pid,
             ka.usename AS blocking_user,
-            a.query AS blocked_query,
-            age(now(), a.query_start) AS blocked_duration
+            ka.state AS state_of_blocking_process,
+            ka.query AS current_or_recent_query_in_blocking_process,
+            age(now(), ka.query_start) AS blocking_duration
           FROM
             pg_catalog.pg_locks bl
-          JOIN
+          LEFT JOIN
             pg_catalog.pg_stat_activity a ON a.pid = bl.pid
-          JOIN
+          LEFT JOIN
             pg_catalog.pg_locks kl ON kl.transactionid = bl.transactionid AND kl.pid != bl.pid
-          JOIN
+          LEFT JOIN
             pg_catalog.pg_stat_activity ka ON ka.pid = kl.pid
           WHERE
             NOT bl.GRANTED
