05462193d3116657d7d6653be73c2b36482418da
Andrew Kane <acekane1@gmail.com>
2017-07-17 08:56:19 -0700

    Consistent sizes

--- lib/pghero/methods/indexes.rb
+++ lib/pghero/methods/indexes.rb
@@ -75,6 +75,7 @@ module PgHero
             relname AS table,
             indexrelname AS index,
             pg_size_pretty(pg_relation_size(i.indexrelid)) AS index_size,
+            pg_relation_size(i.indexrelid) AS size_bytes,
             idx_scan as index_scans
           FROM
             pg_stat_user_indexes ui
