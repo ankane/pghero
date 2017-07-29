2d45de8c6ca82a6377e942be5888972066fdbc4a
Andrew Kane <acekane1@gmail.com>
2017-07-29 13:20:37 -0700

    Updated replica methods

--- lib/pghero/methods/replication.rb
+++ lib/pghero/methods/replication.rb
@@ -3,14 +3,14 @@ module PgHero
     module Replication
       def replica?
         unless defined?(@replica)
-          @replica = select_all("SELECT pg_is_in_recovery()").first["pg_is_in_recovery"]
+          @replica = select_one("SELECT pg_is_in_recovery()")
         end
         @replica
       end
 
       # http://www.postgresql.org/message-id/CADKbJJWz9M0swPT3oqe8f9+tfD4-F54uE6Xtkh4nERpVsQnjnw@mail.gmail.com
       def replication_lag
-        select_all(<<-SQL
+        select_one <<-SQL
           SELECT
             CASE
               WHEN pg_last_xlog_receive_location() = pg_last_xlog_replay_location() THEN 0
@@ -18,18 +18,16 @@ module PgHero
             END
           AS replication_lag
         SQL
-        ).first["replication_lag"]
       end
 
       def replication_slots
-        select_all(<<-SQL
+        select_all <<-SQL
           SELECT
             slot_name,
             database,
             active
           FROM pg_replication_slots
         SQL
-        )
       end
 
       def replicating?
