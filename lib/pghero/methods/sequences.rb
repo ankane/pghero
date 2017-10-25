3e5e33f94899431b1f8b65c9695800197b30eda6
Andrew Kane <acekane1@gmail.com>
2017-10-24 21:46:48 -0700

    Greatly simplified sequences

--- lib/pghero/methods/sequences.rb
+++ lib/pghero/methods/sequences.rb
@@ -2,35 +2,36 @@ module PgHero
   module Methods
     module Sequences
       def sequences
+        # get columns with default values
         sequences = select_all <<-SQL
           SELECT
-            ns.nspname AS schema,
             n.nspname AS table_schema,
             c.relname AS table,
             attname AS column,
             format_type(a.atttypid, a.atttypmod) AS column_type,
-            CASE WHEN format_type(a.atttypid, a.atttypmod) = 'integer' THEN 2147483647::bigint ELSE (pg_sequence_parameters(s.oid)).maximum_value::bigint END AS max_value,
-            s.relname AS sequence
+            pg_get_expr(d.adbin, d.adrelid) AS default_value
           FROM
             pg_catalog.pg_attribute a
           INNER JOIN
             pg_catalog.pg_class c ON c.oid = a.attrelid
           INNER JOIN
             pg_catalog.pg_namespace n ON n.oid = c.relnamespace
-          LEFT JOIN
+          INNER JOIN
             pg_catalog.pg_attrdef d ON (a.attrelid, a.attnum) = (d.adrelid,  d.adnum)
-          INNER JOIN
-            pg_catalog.pg_class s ON s.relkind = 'S'
-          INNER JOIN
-            pg_catalog.pg_namespace ns ON ns.oid = s.relnamespace
           WHERE
             NOT a.attisdropped
             AND a.attnum > 0
             AND d.adsrc LIKE 'nextval%'
-            AND regexp_replace(d.adsrc, '^nextval\\(''(.*)''\\:\\:regclass\\)$', '\\1') IN (s.relname, ns.nspname || '.' || s.relname)
-          ORDER BY
-            s.relname ASC
         SQL
+
+        # parse out sequence
+        sequences.each do |column|
+          m = /^nextval\('(.+)'\:\:regclass\)$/.match(column.delete(:default_value))
+          column[:schema], column[:sequence] = unquote_ident(m[1])
+          column[:max_value] = column[:column_type] == 'integer' ? 2147483647 : 9223372036854775807
+        end
+
+        p sequences
 
         select_all(sequences.map { |s| "SELECT last_value FROM #{quote_ident(s[:schema])}.#{quote_ident(s[:sequence])}" }.join(" UNION ALL ")).each_with_index do |row, i|
           sequences[i][:last_value] = row[:last_value]
@@ -41,6 +42,17 @@ module PgHero
 
       def sequence_danger(threshold: 0.9)
         sequences.select { |s| s[:last_value] / s[:max_value].to_f > threshold }.sort_by { |s| s[:max_value] - s[:last_value] }
+      end
+
+      private
+
+      def unquote_ident(value)
+        schema, seq = value.split(".")
+        unless seq
+          seq = schema
+          schema = 'public'
+        end
+        [unquote(schema), unquote(seq)]
       end
     end
   end
