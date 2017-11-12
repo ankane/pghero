54331dc8dd4f3232eeee3878183945fc500db71c
Andrew <acekane1@gmail.com>
2017-11-12 11:42:07 -0800

    Better replication_lag method when not replica [skip ci]

--- lib/pghero/methods/replication.rb
+++ lib/pghero/methods/replication.rb
@@ -13,7 +13,7 @@ module PgHero
         select_one <<-SQL
           SELECT
             CASE
-              WHEN pg_last_xlog_receive_location() = pg_last_xlog_replay_location() THEN 0
+              WHEN NOT pg_is_in_recovery() OR pg_last_xlog_receive_location() = pg_last_xlog_replay_location() THEN 0
               ELSE EXTRACT (EPOCH FROM NOW() - pg_last_xact_replay_timestamp())
             END
           AS replication_lag
