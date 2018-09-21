80960454505d3436abfdc511090fe87b186ac096
Andrew <acekane1@gmail.com>
2018-09-21 04:58:02 -0700

    Cache feature support to minimize errors in logs - #229

--- lib/pghero/methods/replication.rb
+++ lib/pghero/methods/replication.rb
@@ -10,7 +10,7 @@ module PgHero
 
       # https://www.postgresql.org/message-id/CADKbJJWz9M0swPT3oqe8f9+tfD4-F54uE6Xtkh4nERpVsQnjnw@mail.gmail.com
       def replication_lag
-        with_feature_support do
+        with_feature_support(:replication_lag) do
           lag_condition =
             if server_version_num >= 100000
               "pg_last_wal_receive_lsn() = pg_last_wal_replay_lsn()"
@@ -31,7 +31,7 @@ module PgHero
 
       def replication_slots
         if server_version_num >= 90400
-          with_feature_support([]) do
+          with_feature_support(:replication_slots, []) do
             select_all <<-SQL
               SELECT
                 slot_name,
@@ -46,18 +46,26 @@ module PgHero
       end
 
       def replicating?
-        with_feature_support(false) do
+        with_feature_support(:replicating?, false) do
           select_all("SELECT state FROM pg_stat_replication").any?
         end
       end
 
       private
 
-      def with_feature_support(default = nil)
+      def feature_support
+        @feature_support ||= {}
+      end
+
+      def with_feature_support(cache_key, default = nil)
+        # cache feature support to minimize errors in logs
+        return default if feature_support[cache_key] == false
+
         begin
           yield
         rescue ActiveRecord::StatementInvalid => e
           raise unless e.message.start_with?("PG::FeatureNotSupported:")
+          feature_support[cache_key] = false
           default
         end
       end
