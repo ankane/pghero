017d6fd2e6e678f719817db99d7fc0bf002a1e3e
Andrew Kane <andrew@chartkick.com>
2020-07-08 19:52:14 -0700

    Added more space stats tests

--- test/space_test.rb
+++ test/space_test.rb
@@ -1,7 +1,24 @@
 require_relative "test_helper"
 
 class SpaceTest < Minitest::Test
+  def test_database_size
+    assert database.database_size
+  end
+
   def test_relation_sizes
     assert database.relation_sizes
+  end
+
+  def test_table_sizes
+    assert database.table_sizes
+  end
+
+  def test_space_growth
+    # not enabled
+    # assert database.space_growth
+  end
+
+  def test_space_stats_enabled
+    refute database.space_stats_enabled?
   end
 end
