c269e75cec6952491834ed10b55d76270e0dbea2
Andrew Kane <andrew@chartkick.com>
2020-07-08 20:42:07 -0700

    Test capture_space_stats

--- test/space_test.rb
+++ test/space_test.rb
@@ -17,6 +17,17 @@ class SpaceTest < Minitest::Test
     assert database.space_growth
   end
 
+  def test_relation_space_stats
+    assert database.relation_space_stats("cities")
+  end
+
+  def test_capture_space_stats
+    PgHero::SpaceStats.delete_all
+    refute PgHero::SpaceStats.any?
+    assert database.capture_space_stats
+    assert PgHero::SpaceStats.any?
+  end
+
   def test_space_stats_enabled
     assert database.space_stats_enabled?
   end
