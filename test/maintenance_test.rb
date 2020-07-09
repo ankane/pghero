8ea799edd4ef5e459702f607d1116e04b50e908f
Andrew Kane <andrew@chartkick.com>
2020-07-08 20:46:49 -0700

    Improved tests [skip ci]

--- test/maintenance_test.rb
+++ test/maintenance_test.rb
@@ -3,11 +3,11 @@ require_relative "test_helper"
 class MaintenanceTest < Minitest::Test
   def test_transaction_id_danger
     assert database.transaction_id_danger(threshold: 10000000000).any?
-    assert database.transaction_id_danger.empty?
+    assert_equal [], database.transaction_id_danger
   end
 
   def test_autovacuum_danger
-    assert database.autovacuum_danger
+    assert_equal [], database.autovacuum_danger
   end
 
   def test_vacuum_progress
@@ -15,7 +15,7 @@ class MaintenanceTest < Minitest::Test
   end
 
   def test_maintenance_info
-    assert database.maintenance_info
+    assert database.maintenance_info.find { |v| v[:table] == "cities" }
   end
 
   def test_analyze
