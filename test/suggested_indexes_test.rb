c5b4e44da0aad3628de50d6ce988331c3a5169cf
Andrew Kane <andrew@chartkick.com>
2020-07-08 19:53:57 -0700

    Fixed tests on Travis

--- test/suggested_indexes_test.rb
+++ test/suggested_indexes_test.rb
@@ -6,13 +6,17 @@ class SuggestedIndexesTest < Minitest::Test
     PgHero.reset_query_stats
   end
 
+  def test_suggested_indexes_enabled
+    assert database.suggested_indexes_enabled?
+  end
+
   def test_basic
     User.where(email: "person1@example.org").first
-    assert_equal [{table: "users", columns: ["email"]}], PgHero.suggested_indexes.map { |q| q.except(:queries, :details) }
+    assert_equal [{table: "users", columns: ["email"]}], database.suggested_indexes.map { |q| q.except(:queries, :details) }
   end
 
   def test_existing_index
     User.where("updated_at > ?", Time.now).to_a
-    assert_equal [], PgHero.suggested_indexes.map { |q| q.except(:queries, :details) }
+    assert_equal [], database.suggested_indexes.map { |q| q.except(:queries, :details) }
   end
 end
