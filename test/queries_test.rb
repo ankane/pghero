c265b86d01a3d2b9ca69507cfad63fc2b2abaa5d
Andrew Kane <andrew@chartkick.com>
2020-07-26 19:39:37 -0700

    Added test for filter_data

--- test/queries_test.rb
+++ test/queries_test.rb
@@ -5,6 +5,22 @@ class QueriesTest < Minitest::Test
     assert database.running_queries
   end
 
+  def test_filter_data
+    query = "SELECT pg_sleep(1)"
+    t = Thread.new { ActiveRecord::Base.connection.execute(query) }
+    sleep(0.5)
+    assert_equal query, database.running_queries.first[:query]
+
+    begin
+      PgHero.filter_data = true
+      database.remove_instance_variable(:@filter_data)
+      assert_equal "SELECT pg_sleep($1)", database.running_queries.first[:query]
+    ensure
+      t.join
+      PgHero.filter_data = false
+    end
+  end
+
   def test_long_running_queries
     assert database.long_running_queries
   end
