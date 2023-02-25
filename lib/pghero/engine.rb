0dab6bd8ee3de5743086d792f1ef04792077b526
Andrew Kane <andrew@ankane.org>
2023-02-25 09:23:07 -0800

    Improved code [skip ci]

--- lib/pghero/engine.rb
+++ lib/pghero/engine.rb
@@ -5,7 +5,7 @@ module PgHero
     initializer "pghero", group: :all do |app|
       # check if Rails api mode
       if app.config.respond_to?(:assets)
-        if defined?(Sprockets) && Sprockets::VERSION >= "4"
+        if defined?(Sprockets) && Sprockets::VERSION.to_i >= 4
           app.config.assets.precompile << "pghero/application.js"
           app.config.assets.precompile << "pghero/application.css"
           app.config.assets.precompile << "pghero/favicon.png"
