f5b241049fe17a452780a7a806a7af42725f56e2
Andrew Kane <andrew@ankane.org>
2025-03-14 13:59:50 -0700

    Improved sequence tests

--- test/sequences_test.rb
+++ test/sequences_test.rb
@@ -2,7 +2,21 @@ require_relative "test_helper"
 
 class SequencesTest < Minitest::Test
   def test_sequences
-    assert database.sequences.find { |s| s[:sequence] == "cities_id_seq" }
+    seq = database.sequences.find { |s| s[:sequence] == "cities_id_seq" }
+    assert_equal "public", seq[:table_schema]
+    assert_equal "cities", seq[:table]
+    assert_equal "id", seq[:column]
+    assert_equal "bigint", seq[:column_type]
+    assert_equal 9223372036854775807, seq[:max_value]
+    assert_equal "public", seq[:schema]
+    assert_equal "cities_id_seq", seq[:sequence]
+    assert_equal true, seq[:readable]
+  end
+
+  def test_sequences_last_value
+    last_value = database.sequences.to_h { |s| [s[:sequence], s[:last_value]] }
+    assert_equal 50, last_value["states_id_seq"]
+    assert_equal 5000, last_value["users_id_seq"]
   end
 
   def test_sequence_danger
