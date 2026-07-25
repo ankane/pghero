25ba51d3c48863c3a1ecca8ec8a0455d2ca7e30b
Andrew Kane <andrew@ankane.org>
2026-07-25 12:34:01 -0700

    Fixed CI

--- test/controller_test.rb
+++ test/controller_test.rb
@@ -5,10 +5,11 @@ class ControllerTest < ActionDispatch::IntegrationTest
     get pg_hero.root_path
     assert_response :success
     assert_match "long running queries", response.body
+    refute_match "hit rate", response.body
 
     get pg_hero.root_path(extended: true)
     assert_response :success
-    assert_match "cache hit rate", response.body
+    assert_match "hit rate", response.body
   end
 
   def test_space
